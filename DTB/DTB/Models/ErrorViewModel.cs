09ac69525948b07c7c06d586831adf985ff8c5d4
Asen Karadzhov <asen__7@abv.bg>
2024-02-22 17:04:35 +0200

    Initial create

---
+++ DTB/DTB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DTB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
