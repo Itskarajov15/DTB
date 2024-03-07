5b0b2a7baa7c6b0dc3746958c00ccaeec0cb33a4
Asen Karadzhov <asen__7@abv.bg>
2024-03-07 18:01:45 +0200

    Added category entity configuration

--- DTB/DTB.Infrastructure/Data/ApplicationDbContext.cs
+++ DTB/DTB.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,6 +1,9 @@
-﻿using DTB.Infrastructure.Data.Entities;
+﻿using DTB.Infrastructure.Data.Configurations;
+using DTB.Infrastructure.Data.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
 
 namespace DTB.Infrastructure.Data
 {
@@ -34,64 +37,9 @@ namespace DTB.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Category>().HasData(
-                new List<Category>
-                {
-                    new Category
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Name = "Architecture, Construction",
-                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_architecture_gqdlbl.png",
-                    },
-                    new Category
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Name = "Health and Pharmacy",
-                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_health_nhktmm.png",
-                    },
-                    new Category
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Name = "Drivers, Couriers",
-                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_derivers_q8g7p3.png",
-                    },
-                    new Category
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Name = "Restaurants, Bars, Hotels, Tourism",
-                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824823/icon_restaurant_ee4ubg.png",
-                    },
-                    new Category
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Name = "Engineers and Technicians",
-                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_engineer_wzjia1.png",
-                    },
-                    new Category
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Name = "Human Resources",
-                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_hr_jbc5jn.png",
-                    },
-                    new Category
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Name = "Media, Publishing",
-                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_media_mpzkez.png",
-                    },
-                    new Category
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Name = "Logistics, Spedition",
-                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_logistic_ktvoqa.png",
-                    },
-                    new Category
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Name = "Logistics, Spedition",
-                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_logistic_ktvoqa.png",
-                    }
-                });
+            new CategoryEntityConfiguration().Configure(builder.Entity<Category>());
+
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ DTB/DTB.Infrastructure/Data/Configurations/CategoryEntityConfiguration.cs
@@ -0,0 +1,161 @@
+﻿using DTB.Infrastructure.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DTB.Infrastructure.Data.Configurations
+{
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new List<Category>
+                {
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Architecture, Construction",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_architecture_gqdlbl.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Health and Pharmacy",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_health_nhktmm.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Drivers, Couriers",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_derivers_q8g7p3.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Restaurants, Bars, Hotels, Tourism",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824823/icon_restaurant_ee4ubg.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Engineers and Technicians",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_engineer_wzjia1.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Human Resources",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_hr_jbc5jn.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Media, Publishing",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_media_mpzkez.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Logistics, Spedition",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_logistic_ktvoqa.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Customer support",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_customersupport_fbvie5.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Banking, Lending, Insurance",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_banking_dkgjma.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Marketing, Advertising, PR",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_hr_jbc5jn.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Office and Business activities",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_office_xkdjih.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Energy, Water and Sewage, Utilities",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_energy_kznsrc.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Telecommunications - engineers and technicians",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824825/icon_telecoms_sbsq5u.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Education, Courses, Translators",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_education_kdsm7z.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Security",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824825/icon_security_c4fjq0.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Real-estate",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_realestate_dkhhfe.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Legal",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824821/icon_legal_ofzofx.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Aviation, Airport & Airline",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_airline_bgvmiu.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Auto Service",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824819/icon_autoservice_iabxkj.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Retail, Wholesale",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824824/icon_retail_pgicm9.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Technical Support",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824825/icon_techsupport_zph0mx.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Research and development",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824822/icon_research_mdrafi.png",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Accouting, Audit, Finance",
+                        PictureUrl = "https://res.cloudinary.com/dftfu5p0r/image/upload/v1709824820/icon_finance_r5bftt.png",
+                    },
+                });
+        }
+    }
+}
