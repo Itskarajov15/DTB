a5871e4b41c4a8f84da90526a7a01523dd4f3fb2
Plamena <plamena3456@abv.bg>
2024-04-08 22:56:11 +0300

    Maked changes.

--- DTB/DTB/Controllers/AccountController.cs
+++ DTB/DTB/Controllers/AccountController.cs
@@ -1,18 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DTB.Controllers
 {
     public class AccountController : BaseController
     {
         [HttpGet]
+        [AllowAnonymous]
         public IActionResult UserRegister()
         {
+            //ViewData["Title"] = "User Register";
             return View();
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public IActionResult CompanyRegister()
         {
+            //ViewData["Title"] = "Company Register";
             return View();
         }
     }
