91a9cb8389ccd1cfe5504bbc781b3612dfdda118
Asen Karadzhov <asen__7@abv.bg>
2024-04-08 22:22:26 +0300

    Made changes

---
+++ DTB/DTB.Infrastructure/Data/Configurations/UserRoleEntityConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DTB.Infrastructure.Data.Configurations
+{
+    public class UserRoleEntityConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(new List<IdentityRole>() {
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER",
+                    Id = Guid.NewGuid().ToString()
+                },
+                new IdentityRole
+                {
+                    Name = "Company",
+                    NormalizedName = "COMPANY",
+                    Id = Guid.NewGuid().ToString()
+                },
+                new IdentityRole
+                {
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR",
+                    Id = Guid.NewGuid().ToString()
+                }
+            });
+        }
+    }
+}
